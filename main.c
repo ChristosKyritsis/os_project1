9ecaed90c3373e332b768ed657d8b85d3615f649
ChristosKyritsis <xristoskyrxk@gmail.com>
2023-11-08 15:16:44 +0200

    some changes and empty makefile

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 
-int main() {
+int main(int argc, char** argv) {
+    if (argc == 1) {
+        printf("No arguments were given\n");
+    }
+    else {
+        printf(argv[1], "\n");
+    }
     return 0;
 }
